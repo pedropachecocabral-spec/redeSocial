6a664861db3cd4735d933206c55b8da199d4e4d2
pacheco <pedro.pacheco.cabral@escola.pr.gov.br>
2026-08-06 12:14:59 -0300

    ok

---
+++ script.js
@@ -0,0 +1,9 @@
+//botão curtida 
+
+document.addEventListener("DOMcontentLoaded",()=>{
+const likeBtn = document.querySelector(".left-actions .actions-btn:first-child");
+if(!likeBtn) return;
+const likeSvg = likeBtn.querySelector("svg")
+}
+
+
