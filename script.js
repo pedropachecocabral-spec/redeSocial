d650b2408854a382367f32c2148a1d630160a716
pacheco <pedro.pacheco.cabral@escola.pr.gov.br>
2026-08-13 12:16:20 -0300

    ok

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMcontentLoaded",()=>{
 const likeBtn = document.querySelector(".left-actions .actions-btn:first-child");
 if(!likeBtn) return;
 const likeSvg = likeBtn.querySelector("svg")
-}
 
+//localiza o contador
+
+ let textNode =array.from(likeBtn.childNodes).find(node)=> node.Type
+}
 
